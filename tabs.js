22e138bd1f6f3c4d572a250ef6d24c900f39e40d
Luke Moody <lukemoody@lukes-mbp-lmwd.mynet>
2018-11-21 22:03:11 +0000

    JS tabs linked up

--- tabs.js
+++ tabs.js
@@ -1,1 +1,20 @@
 console.log('log from tabs.js');
+
+function openTab(evt, tabName) {
+
+    var i, tabPanel, tabLinks;
+
+    tabPanel = document.getElementsByClassName("tab-panel");
+    for (i = 0; i < tabPanel.length; i++) {
+      tabPanel[i].style.display = "none";
+    }
+
+    tabLinks = document.getElementsByClassName("tab-link");
+    for (i = 0; i < tabLinks.length; i++) {
+      tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+
+}
